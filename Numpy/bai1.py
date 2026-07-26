fe7f878a0a6addf84b6abc3975d0d89f3c3b0c21
Ryo <dan2007hito@gmail.com>
2026-07-26 21:55:34 +0700

    Ghi lai bai numpy

---
+++ Numpy/bai1.py
@@ -0,0 +1,24 @@
+import numpy as np
+a = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# print(a)
+# print(a[:]) # Lấy hết
+
+# print(a[1]) # Theo vị trí
+# print(a[[0, 2]]) 
+
+# print(a[-1]) # Cuối cùng
+# print(a[:2]) # Lấy số luợng phần tử 
+
+# print(a[2][1]) # Lấy trong mảng 2, vị trí 1
+# print(a[2, 1]) # Giống cái trên
+
+# print(a[1:, 1:]) # Lấy các phần tử của các mảng từ 1 và lấy phần tử có vị trí từ 1
+
+arr = np.arange(100) # Tao 1 mang co 100 phan tu bat dau tu 0
+# arrMask = ((arr % 5) == 0) # Chia het cho 5 thi true
+# arrMask = (((arr % 5) == 0) & (arr > 0)) # Thêm điều kiện
+# print(arr[arrMask]) #Lấy các phần tử vị trí true
+
+test_a = np.array([1, 4, 0, 2, 3, 8, 9, 7])
+print(test_a>3)
---
+++ Numpy/bai2.py
@@ -0,0 +1,19 @@
+import numpy as np
+arr=np.arange(15).reshape((3, 5))
+print(arr)
+print(arr.T) # Chuyen hanh thanh cot, cot thanh hang
+
+arr=np.random.randn(6, 3)
+print(arr)
+
+print('--------------------------')
+np.dot(arr.T, arr) #Nhân ma trận
+arr=np.arange(16).reshape(2, 2, 4) 
+print(arr)
+# print(arr.transpose((1, 0, 2))) 
+# Hoán đổi trục:
+# (1,0) → đổi hàng ↔ cột (đối với mảng 2 chiều).
+# (1,0,2) → đổi trục 0 và trục 1, trục 2 giữ nguyên.
+# (2,1,0) → đưa trục 2 lên đầu, rồi đến trục 1, cuối cùng là trục 0.
+print('--------------------------')
+print(arr.swapaxes(1, 2))
